41a47d505a29993a8d49898ebf950c2ff7cc32ab
Faisal Iqbal <faisal95iqbal@gmail.com>
2017-04-14 02:31:31 +0500

    admin portal update and index page merge

--- PHP/index.php
+++ PHP/index.php
@@ -52,7 +52,7 @@
 						 <li class="dropdown">
                         <a href="#" data-toggle="dropdown" class="dropdown-toggle">Login <b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                            <li><a href="StudentPortal/Logout.php">Student Login</a></li>
+                            <li><a href="St">Student Login</a></li>
                             <li><a href="ManagementPortal/OfficeLogin.php">Management Login</a></li>
                             <li><a href="ParentPortal/ParentLogin.php">Parent Login</a></li>
                         </ul>
