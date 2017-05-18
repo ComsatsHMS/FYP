8f4924b52cb8769b195bcd8537ac01218bfdfe7b
AhmadMukhtar <ahmedmukhtar1133@gmail.com>
2017-05-18 20:37:40 +0500

    New Laptop Cont.

--- PHP/StudentPortal/Complaints_Processing.php
+++ PHP/StudentPortal/Complaints_Processing.php
@@ -36,30 +36,44 @@ function getComplainDetails(){
     $date = $_SESSION['date'];
     global $start;global $end,$query;
     $check =  $_SESSION['id'];
+    global $connection;
 
-global $connection;
+
+    $query = mysqli_query($connection, "select * from wingproctorslist where studentID='$check'");
+    while ($each_record = mysqli_fetch_array($query)) {
+        $start = $each_record['start'];
+        $end = $each_record['end'];
+    }
+    $query = mysqli_query($connection, "select hex('$start')");
+    $row   = mysqli_fetch_row($query);
+    $start = $row[0];
+    $query = mysqli_query($connection, "select hex('$end')");
+    $row   = mysqli_fetch_row($query);
+    $end   = $row[0];
+
     if(isset($_SESSION['type'])){
-        $query = mysqli_query($connection,"select c.*,s.studentName,s.room,s.studentHostel from complaints c,insertstudentprofile s where c.studentid=s.studentid AND c.ComplainType='$type' ORDER BY  ComplainID desc limit 5");
+        $run= mysqli_query($connection,"select c.*,s.studentName,s.room,s.studentHostel from complaints c,insertstudentprofile s where c.studentid=s.studentid AND hex(s.room)>=$start AND hex(s.room) <=$end AND c.ComplainType='$type' ORDER BY  ComplainID desc limit 5");
         unset($_SESSION['type']);
     }
 
   else if(isset($_SESSION['date'])){
-      $query = mysqli_query($connection,"select c.*,s.studentName,s.room,s.studentHostel from complaints c,insertstudentprofile s where c.studentid=s.studentid AND c.Date like '$date%' ORDER BY  ComplainID desc limit 5");
+      $run = mysqli_query($connection,"select c.*,s.studentName,s.room,s.studentHostel from complaints c,insertstudentprofile s where c.studentid=s.studentid AND hex(s.room)>=$start AND hex(s.room) <=$end AND c.Date like '$date%' ORDER BY  ComplainID desc limit 5");
       unset($_SESSION['date']);
     }
     else{
-        $query = mysqli_query($connection,"select w.*,c.* from complaints c,wingprocotrslist w where w.start>=c.room and w.end <=c.room ORDER BY  c.ComplainID desc limit 5");
+        $run = mysqli_query($connection, "select c.*,s.studentName,s.room,s.studentHostel from complaints c,insertstudentprofile s where c.studentid=s.studentid AND hex(s.room)>=$start AND hex(s.room) <=$end ORDER BY  ComplainID desc limit 5");
     }
-    while ($each_record = mysqli_fetch_array($query)) {
-        $Student_ID = $each_record['studentid'];
-        $Complain_ID = $each_record['ComplainID'];
-        $Complain_Type = $each_record['ComplainType'];
-        $Complain_Text = $each_record['ComplainText'];
-        $Complain_Date = $each_record['Date'];
-        $Student_Name = $each_record['studentName'];
-        $Room_No = $each_record['room'];
-        $Hostel = $each_record['studentHostel'];
-                echo "
+
+         while ($each_record = mysqli_fetch_array($run)) {
+            $Student_ID = $each_record['studentid'];
+            $Complain_ID = $each_record['ComplainID'];
+            $Complain_Type = $each_record['ComplainType'];
+            $Complain_Text = $each_record['ComplainText'];
+            $Complain_Date = $each_record['Date'];
+            $Student_Name = $each_record['studentName'];
+            $Room_No = $each_record['room'];
+            $Hostel = $each_record['studentHostel'];
+            echo "
                                                                 <tr><td> $Complain_ID </td>
                                                                 <td> $Student_Name </td>
                                                                 <td> $Room_No </td>
@@ -69,8 +83,8 @@ global $connection;
                                                                 <td> <button type='button' id='view'><a href='ComplainsDisplay.php?id=$Student_ID & room=$Room_No & name=$Student_Name & text=$Complain_Text' >View</a> </button> </td>
                                                                 </tr>
                                                             ";
-            }
-}
+        }
+    }
 
 
 
